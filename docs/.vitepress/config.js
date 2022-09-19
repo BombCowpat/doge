a805616f33b54971db367d608613502b8a17a2b3
chenyanfei <15873624283@163.com>
2022-09-19 14:45:50 +0800

    docs: 三体语录

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -16,11 +16,13 @@ export default defineConfig({
     sidebar: [
       {
         text: 'Guide',
+        collapsible: true,
+        collapsed: true,
         items: [
           { text: '阅读', link: '/guide/getting-started' },
           { text: '青玉案·元夕', link: '/poetries/p1' },
           { text: '沁园春·雪', link: '/poetries/p2' },
-          { text: '博客1', link: '/blogs/b1' },
+          { text: '三体语录', link: '/blogs/b1' },
           { text: '代码规范', link: 'blogs/b2' },
           { text: 'three.js 案例', link: 'blogs/b3' },
           { text: 'OpenCV', link: 'blogs/b4' }
